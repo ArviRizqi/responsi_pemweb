cd87be806b9445a508b9412fd55798a77bc5699c
ArviRizqi <arvirf6013@gmail.com>
2024-07-04 14:12:35 +0700

    push dua

--- components/SidebarSection.vue
+++ components/SidebarSection.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- profil -->
-  <div class="card  sticky-sidebar">
+  <div class="card sticky-sidebar">
     <div class="position-sticky">
       <div class="w-full pt-4 relative">
         <img
@@ -10,7 +10,9 @@
         />
       </div>
 
-      <div class="card-body d-flex flex-column align-items-center text-center mt-24">
+      <div
+        class="card-body d-flex flex-column align-items-center text-center mt-24"
+      >
         <div class="mt-3">
           <h4>Arvi Rizqi Fadhila</h4>
           <p class="text-secondary mb-1">Junior Web Developer</p>
@@ -153,9 +155,9 @@
     <div class="card">
       <ul class="list-group list-group-flush">
         <li
-          class="list-group-item d-flex justify-content-between align-items-center flex-wrap"
+          class="list-group-item d-flex justify-content-between align-items-center flex-wrap py-3 px-4"
         >
-        <svg
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
@@ -173,10 +175,10 @@
               d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"
             ></path>
           </svg>
-          <span class="text-secondary">https://bootdey.com</span>
+          <span class=" text-lg font-semibold"><a href="#" class=" text-secondary">https://dummy.com</a></span>
         </li>
         <li
-          class="list-group-item d-flex justify-content-between align-items-center flex-wrap"
+          class="list-group-item d-flex justify-content-between align-items-center flex-wrap py-3 px-4"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -188,16 +190,16 @@
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="feather feather-github mr-2 icon-inline"
+            class="feather feather-phone mr-2 icon-inline text-success"
           >
             <path
-              d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
-            ></path>
+              d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"
+            />
           </svg>
-          <span class="text-secondary">bootdey</span>
+          <span class="text-secondary text-lg font-semibold"><span>+62</span> 123 4567 8910</span>
         </li>
         <li
-          class="list-group-item d-flex justify-content-between align-items-center flex-wrap"
+          class="list-group-item d-flex justify-content-between align-items-center flex-wrap py-3 px-4"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -209,16 +211,17 @@
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="feather feather-twitter mr-2 icon-inline text-info"
+            class="feather feather-inbox mr-2 icon-inline text-primary"
           >
+            <polyline points="22 12 16 12 14 15 10 15 8 12 2 12" />
             <path
-              d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
-            ></path>
+              d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"
+            />
           </svg>
-          <span class="text-secondary">@bootdey</span>
+          <span class="text-secondary text-lg font-semibold">arvi.rizqi123@gmail.com</span>
         </li>
         <li
-          class="list-group-item d-flex justify-content-between align-items-center flex-wrap"
+          class="list-group-item d-flex justify-content-between align-items-center flex-wrap py-3 px-4"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -230,34 +233,12 @@
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="feather feather-instagram mr-2 icon-inline text-danger"
+            class="feather feather-map-pin text-danger mr-2 icon-inline"
           >
-            <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
-            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-            <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+            <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" />
+            <circle cx="12" cy="10" r="3" />
           </svg>
-          <span class="text-secondary">bootdey</span>
-        </li>
-        <li
-          class="list-group-item d-flex justify-content-between align-items-center flex-wrap"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="feather feather-facebook mr-2 icon-inline text-primary"
-          >
-            <path
-              d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"
-            ></path>
-          </svg>
-          <span class="text-secondary">bootdey</span>
+          <span class="text-secondary text-lg font-semibold">Yogyakarta, Indonesia</span>
         </li>
       </ul>
     </div>
@@ -277,8 +258,8 @@ export default {
 }
 
 .sticky-sidebar {
-      position: -webkit-sticky;
-      position: sticky;
-      top: 220px; /* Height of navbar */
+  position: -webkit-sticky;
+  position: sticky;
+  top: 220px; /* Height of navbar */
 }
 </style>
